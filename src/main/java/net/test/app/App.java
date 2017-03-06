3e4be9cd7e9db8a744de01755245b3d8007d1f0f
root <root@localhost.localdomain>
2017-03-06 20:41:38 +0200

    Add dependency

--- src/main/java/net/test/app/App.java
+++ src/main/java/net/test/app/App.java
@@ -4,10 +4,14 @@ package net.test.app;
  * Hello world!
  *
  */
+import org.joda.time.LocalTime;
+
 public class App 
 {
     public static void main( String[] args )
     {
+	LocalTime currentTime = new LocalTime();
+	System.out.println("The current local time is: " + currentTime);
         System.out.println( "Hello World!" );
     }
 }
